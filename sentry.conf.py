0b03940f71aa000a3800cfc8e186c801fed26e73
roleyfoley <roleyfoley@gmail.com>
2018-08-06 14:05:34 +1000

    Add KMS Decryption and sentry-plugins

--- sentry.conf.py
+++ sentry.conf.py
@@ -37,10 +37,65 @@ from sentry.conf.server import *  # NOQA
 
 import os
 import os.path
+import boto3
+
+import base64
+import logging
 
 CONF_ROOT = os.path.dirname(__file__)
 
-postgres = env('SENTRY_POSTGRES_HOST') or (env('POSTGRES_PORT_5432_TCP_ADDR') and 'postgres')
+###
+# AWS KMS Utilities
+#
+# if we are in AWS, some environment variables may be in KMS
+###
+
+BASE64_PREFIX = 'base64:'
+AWS_REGION = os.environ.get('AWS_REGION')
+
+def decrypt_kms_data(encrypted_data):
+    """Decrypt KMS encoded data."""
+    if not AWS_REGION:
+        return
+
+    kms = boto3.client('kms', region_name=AWS_REGION)
+
+    decrypted = kms.decrypt(CiphertextBlob=encrypted_data)
+
+    if decrypted.get('KeyId'):
+        # Decryption succeed
+        decrypted_value = decrypted.get('Plaintext', '')
+        return decrypted_value
+
+
+def string_or_b64kms(value):
+    """Check if value is base64 encoded - if yes, decode it using KMS."""
+    if not value:
+        return value
+
+    try:
+        # Check if environment value base64 encoded
+        if isinstance(value, str) and value.startswith(BASE64_PREFIX):
+            value = value[len(BASE64_PREFIX):]
+            # If yes, decode it using AWS KMS
+            data = base64.b64decode(value)
+            decrypted_value = decrypt_kms_data(data)
+
+            # If decryption succeed, use it
+            if decrypted_value:
+                value = decrypted_value
+    except Exception as e:
+        logging.exception(e)
+    return value
+
+
+def env(field_name, default=None):
+    raw_value = os.environ.get(field_name, default)
+    if raw_value:
+        return string_or_b64kms(raw_value)
+    return raw_value
+
+postgres = env('SENTRY_DB_HOST') or (env('POSTGRES_PORT_5432_TCP_ADDR') and 'postgres')
 if postgres:
     DATABASES = {
         'default': {
@@ -62,7 +117,7 @@ if postgres:
             ),
             'HOST': postgres,
             'PORT': (
-                env('SENTRY_POSTGRES_PORT')
+                env('SENTRY_DB_PORT')
                 or ''
             ),
             'OPTIONS': {
