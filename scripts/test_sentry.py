5ad3a76646ac5d2fba2e83d456cc15796f7a963b
roleyfoley <roleyfoley@gmail.com>
2019-11-22 12:05:59 +1100

    update test sentry to include level

--- scripts/test_sentry.py
+++ scripts/test_sentry.py
@@ -1,12 +1,22 @@
 import sentry_sdk
 import os
 
+from sentry_sdk import configure_scope
+
+# Allows you to test most of the sentry formatting setups
+SENTRY_DSN=os.environ.get('SENTRY_DSN')
+SENTRY_ENVIRONMENT=os.environ.get('SENTRY_ENVIRONMENT', default=None)
+SENTRY_TEST_LEVEL=os.environ.get('SENTRY_TEST_LEVEL', default='warning' )
+SENTRY_TEST_MESSAGE=os.environ.get('SENTRY_TEST_MESSAGE', default='This is an example of an error message.')
+
 sentry_sdk.init(
-    dsn=os.environ.get('SENTRY_DSN'),
-    environment=os.environ.get('SENTRY_ENVIRONMENT', default='unknown'),
+    dsn=SENTRY_DSN,
+    environment=SENTRY_ENVIRONMENT,
 )
+
+with configure_scope() as scope:
+    scope.level = SENTRY_TEST_LEVEL
+
 sentry_sdk.capture_exception(
-    Exception(
-        os.environ.get('SENTRY_TEST_MESSAGE', default='This is an example of an error message.')
-    )
+    Exception(SENTRY_TEST_MESSAGE)
 )
